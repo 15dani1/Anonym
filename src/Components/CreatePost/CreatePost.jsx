1aa392da22859992903b994530bcfee1581e275b
Rohil Tuli <rohil.tuli@gmail.com>
2020-05-23 17:04:55 -0400

    Worked on create post page

--- src/Components/CreatePost/CreatePost.jsx
+++ src/Components/CreatePost/CreatePost.jsx
@@ -1,11 +1,23 @@
 import React, { useState } from 'react';
-import { Switch, SegmentedControl, Textarea, Tablist, Paragraph, Pane, Label, Tab } from 'evergreen-ui'
+import { Radio, Input, Layout, Space, Button } from 'antd'
+import { FormOutlined, EditOutlined } from '@ant-design/icons'
+
+import 'antd/dist/antd.css'
 
 const CreatePost = (props) => {
     const [previewMd, setPreviewMd] = useState(false)
+    const [postTitle, setPostTitle] = useState("")
+    const { Header, Content, Footer } = Layout;
+    const { TextArea } = Input;
     const tabs = ['Markdown', 'Render']
     return (
         <div>
+            <Layout className="feed" hasSider={false} style={{
+                'margin': 'auto',
+                'background-color': "white"}}>
+                <Header style={{'background-color': "white"}}>
+                <div className="postTitle">Create New Post</div>
+                </Header>
             {/* <SegmentedControl 
                 options={[{ label: 'Markdown', value: false }, { label: 'Render', value: true }]}
                 width={160}
@@ -13,49 +25,22 @@ const CreatePost = (props) => {
                 value={previewMd}
                 onChange={value => setPreviewMd(value)}
             /> */}
-            <Switch height={25}/>
-            <Pane height={120}>
-                <Tablist marginBottom={16} flexBasis={240} marginRight={24}>
-            {tabs.map((tab, index) => (
-            <Tab
-                key={tab}
-                id={tab}
-                onSelect={() => setPreviewMd(tab == 'Render')}
-                isSelected={true}
-                aria-controls={`panel-${tab}`}
-            >
-                {tab}
-            </Tab>
-            ))}
-        </Tablist>
-            <Pane padding={16} background="tint1" flex="1">
-                {tabs.map((tab, index) => (
-                <Pane
-                    key={tab}
-                    id={`panel-${tab}`}
-                    role="tabpanel"
-                    aria-labelledby={tab}
-                >
-                    <Paragraph>Panel {tab}</Paragraph>
-                </Pane>
-                ))}
-            </Pane>
-            </Pane>
-            {previewMd ? <div>Nothing yet</div> :
-            <Pane>
-                <Label
-                    htmlFor="textarea-2"
-                    marginBottom={4}
-                    display="block"
-                >
-                    Label
-                </Label>
-                <Textarea
-                    id="textarea-2"
-                    placeholder="Textarea placeholder..."
-                />
-            </Pane>
-            }
+            <Content style={{'margin': 'auto'}}>
+            <Space direction="vertical" size="large">
+
+            <Input size="large" placeholder="Post Title" prefix={<FormOutlined/>} style={{width: 400}}/>
+            <Radio.Group defaultValue="Markdown" buttonStyle="solid">
+                <Radio.Button value="Markdown">Markdown</Radio.Button>
+                <Radio.Button value="Render">Render</Radio.Button>
+            </Radio.Group>
+            <TextArea
+            placeholder="Enter Text Here" style={{width: 800}}
+            autoSize={{ minRows: 10, maxRows: 30 }}
+            />
+            <Button size="large">Submit</Button>
+            </Space>
+            </Content>
+            </Layout>
         </div>
     )
 }

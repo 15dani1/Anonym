f8f0d9461e48b76eb9ee350f8218c51ecd7012a3
Anmol Parande <parandea1.7@gmail.com>
2020-05-24 10:54:54 -0500

    Updates

---
+++ precache-manifest.c7ed15fd01b9be74b09899598747c3cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19a1cfd5107f72b0862447e722762e8d",
+    "url": "/Open-Hacks/index.html"
+  },
+  {
+    "revision": "c8ccca570e449b3f939b",
+    "url": "/Open-Hacks/static/css/2.2e7b0b3f.chunk.css"
+  },
+  {
+    "revision": "606b839cc79d5bbb82d1",
+    "url": "/Open-Hacks/static/css/main.3cd973ab.chunk.css"
+  },
+  {
+    "revision": "c8ccca570e449b3f939b",
+    "url": "/Open-Hacks/static/js/2.e10d77f4.chunk.js"
+  },
+  {
+    "revision": "a7c3573b4873fdfd7061d251451eabfa",
+    "url": "/Open-Hacks/static/js/2.e10d77f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "606b839cc79d5bbb82d1",
+    "url": "/Open-Hacks/static/js/main.3abfaa5e.chunk.js"
+  },
+  {
+    "revision": "da646cb5dca15547ffd0",
+    "url": "/Open-Hacks/static/js/runtime-main.5230469d.js"
+  },
+  {
+    "revision": "ef5a97cdd5aa58600df10081296d8038",
+    "url": "/Open-Hacks/static/media/Hat.ef5a97cd.jpg"
+  }
+]);
